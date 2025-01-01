d6508060d1604b5f0ebf95c17f5c5e0e8e5c07f0
Hosein <48409432+Ho3einWave@users.noreply.github.com>
2025-01-02 00:04:53 +0330

    feat: add postcss plugins for rem to pixel and px to viewport conversion

--- web/postcss.config.js
+++ web/postcss.config.js
@@ -2,5 +2,19 @@ export default {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    'postcss-rem-to-pixel': {
+      propList: ['*'],     // Convert all properties (padding, margin, etc.)
+      rootValue: 16, // Match your HTML font size (usually 16px by default in Tailwind)
+      replace: true, // Replace `rem` values with `px`
+    },
+    'postcss-px-to-viewport': {
+      viewportWidth: 1920, // The width of your design
+      unitPrecision: 5,   // Number of decimal places to round to
+      viewportUnit: 'vw', // Convert to `vm` (viewport width unit)
+      selectorBlackList: [], // Add any selectors you don't want to convert
+      minPixelValue: 0,  // Set the minimum `px` value to convert
+      mediaQuery: false, // Ignore media queries (set true if needed)
+
+    },
   },
 }
